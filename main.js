a233e3f76ebfba11b7f5ae75eafbfa7381f5cb65
omics <omics@calu.com>
2023-10-04 12:12:03 -0500

    js 62%

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-function dropdownActivity(opening_event) {
-  let dropdown = opening_event.target.closest(".Dropdown");
+// Actividad del menu desplegable
+function openDropdown(first_event) {
+  let dropdown = first_event.target.closest(".Dropdown");
   if (dropdown && !dropdown.classList.contains("is-open")) {
-    function turnOffListeners() {
+    function closeDropdown() {
       dropdown.classList.remove("is-open");
-      window.removeEventListener("click", closeDropdown);
-      // window.removeEventListener("keydown", selectOption);
+      window.removeEventListener("click", manageDropdown);
       console.log("%ccerrado", "color: lightcoral");
+      // window.removeEventListener("keydown", selectOption);
     }
     //   function selectOption(prm_event_3) {
     //     // Obtiene la lista de opciones del menú desplegable
@@ -53,60 +54,50 @@ function dropdownActivity(opening_event) {
     //     // Selecciona la opción
     //     if (prm_event_3.keyCode === 13) {
     //       options[newIndex].classList.add("is-selected");
-    //       turnOffListeners();
+    //       close();
     //     }
     //   }
-    function closeDropdown(closing_event) {
-      if (dropdown.contains(closing_event.target)) {
-        //       // Cierra el dropdown
-        //       if (
-        //         prm_event_2.target.closest(".Dropdown-toggle .bx") ||
-        //         prm_event_2.target.closest(".Dropdown-menu-header .bx")
-        //       ) {
-        //         turnOffListeners();
-        //       }
-        //       // Selecciona una opción
-        //       const option = prm_event_2.target.closest(
-        //         ".Dropdown-menu-list-option"
-        //       );
-        //       if (option) {
-        //         let arr = [];
-        //         const menu_list = prm_event_2.target.closest(".Dropdown-menu-list");
-        //         menu_list.childNodes.forEach((node_item) => {
-        //           if (node_item.nodeType === 1) arr.push(node_item);
-        //         });
-        //         arr.forEach((element) => {
-        //           element.classList.remove("is-selected");
-        //           if (option == element) {
-        //             element.classList.add("is-selected");
-        //             menu_list.scrollTop = arr.indexOf(element) * 36;
-        //           }
-        //         });
-        //         turnOffListeners();
-        //       }
+    function manageDropdown(second_event) {
+      const element = second_event.target;
+      if (
+        !dropdown.contains(element) ||
+        element.closest(".Dropdown-toggle .bx") ||
+        element.closest(".Dropdown-menu-header .bx")
+      ) {
+        closeDropdown();
       } else {
-        // Cierra el dropdown al dar clic fuera
-        turnOffListeners();
+        const option = element.closest(".Dropdown-menu-list-option");
+        if (option) {
+          dropdown
+            .querySelectorAll(".Dropdown-menu-list-option")
+            .forEach((item) => item.classList.remove("is-selected"));
+          option.classList.add("is-selected");
+          dropdown.querySelector(".Dropdown-toggle-search").value =
+            option.innerText;
+          closeDropdown();
+        }
       }
     }
     dropdown.classList.add("is-open");
-    window.addEventListener("click", closeDropdown);
-    //   window.addEventListener("keydown", selectOption);
+    window.addEventListener("click", manageDropdown);
     console.log("%cabierto", "color: lightgreen");
+    // dropdown.addEventListener("focusout", closeDropdown);
+    // window.addEventListener("keydown", selectOption);
   }
 }
 
-window.addEventListener("click", dropdownActivity);
-window.addEventListener("focusin", dropdownActivity);
+// Escuchadores globales
+window.addEventListener("click", openDropdown);
+window.addEventListener("focusin", openDropdown);
 
 // window.addEventListener("click", (event) => {
 //   let dropdown = event.target.closest(".Dropdown");
 //   console.log(dropdown);
 //   if (dropdown) {
 //     if (!dropdown.classList.contains("is-open")) {
-//       function turnOffListeners() {
+//       function close() {
 //         dropdown.classList.remove("is-open");
-//         window.removeEventListener("click", closeDropdown);
+//         window.removeEventListener("click", manageDropdown);
 //         window.removeEventListener("keydown", selectOption);
 //         console.log("%ccerrado", "color: lightcoral");
 //       }
@@ -163,17 +154,17 @@ window.addEventListener("focusin", dropdownActivity);
 //         // Selecciona la opción
 //         if (prm_event_3.keyCode === 13) {
 //           options[newIndex].classList.add("is-selected");
-//           turnOffListeners();
+//           close();
 //         }
 //       }
-//       function closeDropdown(prm_event_2) {
+//       function manageDropdown(prm_event_2) {
 //         if (dropdown.contains(prm_event_2.target)) {
 //           // Cierra el dropdown
 //           if (
 //             prm_event_2.target.closest(".Dropdown-toggle .bx") ||
 //             prm_event_2.target.closest(".Dropdown-menu-header .bx")
 //           ) {
-//             turnOffListeners();
+//             close();
 //           }
 //           // Selecciona una opción
 //           const option = prm_event_2.target.closest(
@@ -192,15 +183,15 @@ window.addEventListener("focusin", dropdownActivity);
 //                 menu_list.scrollTop = arr.indexOf(element) * 36;
 //               }
 //             });
-//             turnOffListeners();
+//             close();
 //           }
 //         } else {
 //           // Cierra el dropdown al dar clic fuera
-//           turnOffListeners();
+//           close();
 //         }
 //       }
 //       dropdown.classList.add("is-open");
-//       window.addEventListener("click", closeDropdown);
+//       window.addEventListener("click", manageDropdown);
 //       window.addEventListener("keydown", selectOption);
 //       console.log("%cabierto", "color: lightgreen");
 //     }
