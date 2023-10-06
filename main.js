d061e1b16c42c407e37320edb171c89e12ca22e2
Alincut Lein <alain.vg.96@gmail.com>
2023-10-06 01:15:34 -0500

    70% JS - Controlador de clics y teclas + reducción de código

--- main.js
+++ main.js
@@ -13,121 +13,77 @@ function startDropdown(event_1) {
     });
     function close() {
       dropdown.classList.remove("is-open");
-      window.removeEventListener("click", handlingClicks);
-      window.removeEventListener("keydown", handlingKeydowns);
+      window.removeEventListener("click", handlingEvents);
+      window.removeEventListener("keydown", handlingEvents);
       console.log("%ccerrado", "color: lightcoral");
     }
-    function handlingClicks(event_2) {
-      const clicked_element = event_2.target;
-      if (
-        !dropdown.contains(clicked_element) ||
-        clicked_element.closest(".Dropdown-toggle .bx") ||
-        clicked_element.closest(".Dropdown-menu-header .bx")
-      ) {
-        close();
-      } else {
-        const picked_option = clicked_element.closest(
-          ".Dropdown-menu-list-option"
-        );
-        if (picked_option) {
-          option_list.forEach((option) => {
-            option === picked_option
-              ? (option.classList.add("is-selected"),
-                (dropdown.querySelector(".Dropdown-menu-list").scrollTop =
-                  option_list.indexOf(option) * 36))
-              : option.classList.remove("is-selected");
-          });
-          [".Dropdown-toggle-search", 'input[type="hidden"]'].forEach(
-            (class_selector) => {
-              dropdown.querySelector(class_selector).value =
-                picked_option.innerText;
-            }
-          );
-          close();
-        }
+    function handlingEvents(event) {
+      function setClass(element, classname) {
+        option_list.forEach((option) => {
+          option.classList.toggle(classname, option === element);
+        });
+        dropdown.querySelector(".Dropdown-menu-list").scrollTop =
+          option_list.indexOf(element) * 36;
       }
-    }
-    function handlingKeydowns(event_3) {
-      // if (event_3.keyCode === 9) close();
-      // let focused_option_index = option_list.findIndex((option) =>
-      //   option.classList.contains("is-focused")
-      // );
-      // if (event_3.keyCode === 40) focused_option_index++;
-      // if (event_3.keyCode === 38) focused_option_index--;
-      // if (focused_option_index < 0) {
-      //   focused_option_index = option_list.length - 1;
-      // } else if (focused_option_index >= option_list.length) {
-      //   focused_option_index = 0;
-      // }
-      // option_list.forEach((option) => {
-      //   option.classList.toggle(
-      //     "is-focused",
-      //     option === option_list[focused_option_index]
-      //   );
-      // });
-      // dropdown.querySelector(".Dropdown-menu-list").scrollTop =
-      //   focused_option_index * 36;
-      // if (event_3.keyCode === 13) {
-      //   option_list.forEach((option) => {
-      //     option.classList.toggle(
-      //       "is-selected",
-      //       option === option_list[focused_option_index]
-      //     );
-      //   });
-      //   [".Dropdown-toggle-search", 'input[type="hidden"]'].forEach(
-      //     (class_selector) => {
-      //       dropdown.querySelector(class_selector).value =
-      //         option_list[focused_option_index].innerText;
-      //     }
-      //   );
-      //   close();
-      // }
-
-      let focused_option_index = option_list.findIndex((option) =>
-        option.classList.contains("is-focused")
-      );
-      switch (event_3.keyCode) {
-        case 9:
-          close();
-          break;
-        case 40:
-        case 38:
-          focused_option_index =
-            event_3.keyCode === 40
-              ? (focused_option_index + 1) % option_list.length
-              : (focused_option_index - 1 + option_list.length) %
-                option_list.length;
-          option_list.forEach((option) => {
-            option.classList.toggle(
-              "is-focused",
-              option === option_list[focused_option_index]
+      function setValues(element) {
+        [".Dropdown-toggle-search", "input[type='hidden']"].forEach(
+          (class_selector) => {
+            dropdown.querySelector(class_selector).value = element.innerText;
+          }
+        );
+      }
+      switch (event.type) {
+        case "click":
+          if (
+            !dropdown.contains(event.target) ||
+            event.target.closest(".Dropdown-toggle .bx") ||
+            event.target.closest(".Dropdown-menu-header .bx")
+          ) {
+            close();
+          } else {
+            const picked_option = event.target.closest(
+              ".Dropdown-menu-list-option"
             );
-          });
-          dropdown.querySelector(".Dropdown-menu-list").scrollTop =
-            focused_option_index * 36;
-          break;
-        case 13:
-          option_list.forEach((option) => {
-            option.classList.toggle(
-              "is-selected",
-              option === option_list[focused_option_index]
-            );
-          });
-          [".Dropdown-toggle-search", 'input[type="hidden"]'].forEach(
-            (class_selector) => {
-              dropdown.querySelector(class_selector).value =
-                option_list[focused_option_index].innerText;
+            if (picked_option) {
+              setClass(picked_option, "is-selected");
+              setValues(picked_option);
+              close();
             }
-          );
-          close();
+          }
           break;
-        default:
+        case "keydown":
+          let focus_index = option_list.findIndex((option) =>
+            option.classList.contains("is-focused")
+          );
+          switch (event.keyCode) {
+            case 9:
+            case 13:
+              if (event.keyCode === 13 && option_list[focus_index]) {
+                setClass(option_list[focus_index], "is-selected");
+                setValues(option_list[focus_index]);
+              }
+              close();
+              break;
+            case 40:
+            case 38:
+              focus_index = Math.max(
+                0,
+                Math.min(
+                  focus_index + (event.keyCode === 40 ? 1 : -1),
+                  option_list.length - 1
+                )
+              );
+              setClass(option_list[focus_index], "is-focused");
+              break;
+            default:
+              break;
+          }
           break;
       }
     }
     dropdown.classList.add("is-open");
-    window.addEventListener("click", handlingClicks);
-    window.addEventListener("keydown", handlingKeydowns);
+    window.addEventListener("click", handlingEvents);
+    window.addEventListener("keydown", handlingEvents);
     console.log("%cabierto", "color: lightgreen");
   }
 }
